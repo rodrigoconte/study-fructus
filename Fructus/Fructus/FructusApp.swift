a4845513bfb4871fe44a715864616f7f11e896ee
Rodrigo Conte <rodrigo.conte-terceiro@cea.com.br>
2023-12-13 10:06:38 -0400

    Feat: Configuring initial assets and project settings

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rodrigo Conte Oliveira on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
